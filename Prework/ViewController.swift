379dbe981a6c29b6301cca487ca24db800b511f6
crowley-ce <106932285+crowley-ce@users.noreply.github.com>
2022-07-16 22:42:12 -0400

    Added Codepath Prework files to repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Christopher Crowley on 7/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    
+    @IBOutlet weak var UserInput: UITextField!
+    
+  
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+
+    @IBAction func GoodbyeClicked(_ sender: Any) {
+        TextLabel.text = "Goodbye! 👋"
+    }
+    
+  
+    @IBAction func ChangeBackground(_ sender: Any) {
+        self.view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func ChangeText(_ sender: Any) {
+        let mText = UserInput.text
+        TextLabel.text = mText
+    }
+}
+
